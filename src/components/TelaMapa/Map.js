69cc04b27315aeb029e18dd97e887fe4a060a67b
AndreBarbosa <andrevictorbarbosa@hotmail.com>
2025-05-15 11:58:52 -0300

    dfg

--- src/components/TelaMapa/Map.js
+++ src/components/TelaMapa/Map.js
@@ -6,7 +6,7 @@ import { Float } from "@chakra-ui/react";
 
 
 const MapsWithRoute = ({ origin, destination, valorPorKmEncomenda, quantConvidados,quantConvidadosPersonalizado, cepType, kmImperio, kmProfissionais }) => {
-  const apiKey = 'AIzaSyAjsdT4YTOXlV97_vrOGfmoTceCx0Nyr6k';
+  const apiKey = '';
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: apiKey,
 
